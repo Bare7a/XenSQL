2e05bf42cec77c070ceea598041107bb96b70c93
Bare7a <bare7a@gmail.com>
2026-06-21 19:04:34 +0300

    Bumped version to 1.2.2

--- frontend/src/shared/lib/appInfo.ts
+++ frontend/src/shared/lib/appInfo.ts
@@ -10,7 +10,7 @@ export interface AppInfo {
 /** Fallback when Go binding is unavailable (dev in browser). */
 export const DEFAULT_APP_INFO: AppInfo = {
   name: 'XenSQL',
-  version: '1.2.1',
+  version: '1.2.2',
   author: 'Bare7a',
   email: 'bare7a@gmail.com',
   repository: 'https://github.com/Bare7a/XenSQL',
